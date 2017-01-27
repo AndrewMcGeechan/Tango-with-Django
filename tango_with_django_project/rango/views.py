e27abc883637a96dadc7898004968a2e359f5db1
Andrew McGeechan <apmcgeechan@googlemail.com>
2017-01-27 13:50:48 +0000

    Chapter 4 finished

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
+    #Constructs a dictionary to pass to the template as context
+    #boldmessage is the same as {{boldmessage}} in the template
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    #Returns a rendered response to send to the client
+    #Shortcut function makes our lives easier
+    #First parameter is the template we wish to use
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
+    context_dict_about = {'boldmessage': "This tutorial has been put together by Andrew McGeechan"}
+    return render(request, 'rango/about.html', context=context_dict_about)
